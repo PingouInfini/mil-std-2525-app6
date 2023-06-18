4ad071a7bd79ee26e520a231ac75aae936db49a7
PingouInfini <pingouninfini@banquise.org>
2023-06-18 22:01:11 +0200

    Update of app6 image generation process

--- README.md
+++ README.md
@@ -5,20 +5,18 @@
 **[EN] APP6 symbols are available in the directory `APP6-icons`.**  
 **[FR] Les symboles APP6 sont disponibles dans le répertoire `APP6-icons`.**
 
-L'objectif est de récupérer l'ensemble des icones APP6B issus de [milsymbol-APP6b](https://www.spatialillusions.com/milsymbol/docs/milsymbol-APP6b.html)
+L'objectif est de récupérer l'ensemble des codes APP6B issus de [milsymbol-APP6b](https://www.spatialillusions.com/milsymbol/docs/milsymbol-APP6b.html) et des svg présents dans `rawdata/symbols``et de construire un répertoire d'images APP6b
 
 Ces icones seront stockées dans des répertoires selon leur format : `svg` ou `png`  
-Un fichier de mapping permet de raccrocher chaque hierarchie, code symbole (SIDC) à chaque fichier APP6.
+Un fichier de mapping permet de raccrocher chaque hierarchie, code symbole (SIDC) à chaque fichier APP6.  
+Un fichier d'arborescence des codes est également présent dans `APP6-icons`
 
 
 # Récupération
 
 ## Récupération des images
 
-Depuis la page [milsymbol-APP6b](https://www.spatialillusions.com/milsymbol/docs/milsymbol-APP6b.html), à l'aide du plugin de chrome [SVG Export](https://chrome.google.com/webstore/detail/svg-export/naeaaedieihlkmdajjefioajbbdbdjgp?hl=fr)
-
-- On enregistre l'ensemble des images au format `svg` dans le répertoire `rawdata`
-  > MIL-STD-2525-APP6\rawdata\svg\svgexport-[x].svg
+Depuis la page [milsymbol-APP6b](https://www.spatialillusions.com/milsymbol/docs/milsymbol-APP6b.html)
 
 - On enregistre la page web en html -> Page web COMPLETE également dans `rawdata`
   > MIL-STD-2525-APP6\rawdata\Milsymbol APP6-B.html
@@ -30,13 +28,17 @@ Puis :
 python -m pip install --upgrade pip
 pip3 install -r requirements.txt
 pip install lib\cairocffi-1.3.0-cp310-cp310-win_amd64.whl
-python generate-APP6-icons.py
+python extract-APP6-icons.py
 ```
 
 
-## Renommage en python
+## Rannomage/Création des images en python
+
+A partir du fichier **html**, on va créer un csv contenant les informations permettant de mapper la hierarchy, le SIDC et leurs noms respectifs (`icon-files-mapping.csv`), ainsi qu'un fichier d'arborescence des hierarchies (`hierarchy-tree.txt`).  
+A partir du fichier **csv** nouvellement créé, on va itérer sur les symboles (images **svg**) présents dans le répertoire `rawdata/symbols`, les renommer en utilisant la hiérarchie, et les stocker dans le répertoire (`APP6-icons`)  
+Pour terminer, on génère les **png** équivalents.  
 
 Pour chaque **hiérarchie** (ie:`1.X.2.1.1.2.1`), on récupère :
 - le **SIDC** (*Symbol identification coding*) associé (ie: `S*A*MFFI--`)
-- le **nom** (ie `AIR TRACK - MILITARY - FIXED WING - FIGHTER - INTERCEPTOR`)
-- Si la hiérarchie possède une balise `<svg>`, on renomme les fichiers exportés par le plugin par sa hiérarchie
+- le **FullPath** (ie `AIR TRACK - MILITARY - FIXED WING - FIGHTER - INTERCEPTOR`)
+- le **Nom** (ie `INTERCEPTOR`)
--- extract-APP6-icons.py
+++ extract-APP6-icons.py
@@ -1,12 +1,25 @@
 import csv
 import os
-import shutil
+
+from app6.data_generation.data_generation import generate_data_from_milsymbol_app6b
+from app6.utils.file_manager import copy_and_rename_file, clear_files_in_directory
+from app6.utils.image_manager import create_png_equivalents
+
+mapping_file = os.path.join("APP6-icons", "icon-files-mapping.csv")
+symbol_code = {}
 
 
-def read_svg_files():
+def main():
+    generate_data_from_milsymbol_app6b(mapping_file)
+    manage_svg_files()
+    # Creating png images from svg files
+    create_png_equivalents("APP6-icons/svg", "APP6-icons/png")
+
+
+def manage_svg_files():
     global symbol_code
+    global mapping_file
     svg_directory = os.path.join("rawdata", "symbols")
-    mapping_file = os.path.join("rawdata", "APP6_code_mapping.csv")
     codes = {}
 
     # Read the mapping file
@@ -15,9 +28,11 @@ def read_svg_files():
         next(csv_reader)  # Ignore the CSV header
         for line in csv_reader:
             if line:
-                symbol_code = line[1].lower().replace("-", "")
                 code = line[0].lower().replace("-", "")
+                symbol_code = line[1].lower().replace("-", "")
                 codes[symbol_code] = code
+
+    clear_files_in_directory(os.path.join("APP6-icons"), [".png", ".svg"])
 
     # Iterate over the SVG files
     for file_name in os.listdir(svg_directory):
@@ -53,65 +68,13 @@ def read_svg_files():
 
 
 def replace_last_character_found(string, character):
-    last_p_index = string.rfind(character)  # Find the index of the last "p" in the string
-    if last_p_index != -1:  # Check if "p" is present in the string
+    last_p_index = string.rfind(character)  # Find the index of the last "character" in the string
+    if last_p_index != -1:  # Check if "character" is present in the string
         modified_string = string[:last_p_index] + "*" + string[last_p_index + 1:]
         return modified_string
     else:
         return string
 
 
-def copy_and_rename_file(file_in, subfolder, file_out):
-    file_directory = os.path.dirname(file_in)
-
-    # Check if the subfolder exists, create it if it doesn't
-    if not os.path.exists(os.path.join("APP6-icons", "svg", subfolder)):
-        os.makedirs(os.path.join("APP6-icons", "svg", subfolder))
-
-    # Build the full path of the input file
-    file_in_path = os.path.abspath(file_in)
-
-    # Get the file extension
-    file_extension = os.path.splitext(file_in)[1]
-    if os.path.splitext(file_out)[1] == file_extension:
-        file_extension = ""
-
-    # Build the full path of the output file in the subfolder
-    file_out_path = os.path.join("APP6-icons", "svg", subfolder, file_out + file_extension)
-
-    # Copy the input file to the subfolder with the specified output filename
-    shutil.copy2(file_in_path, file_out_path)
-
-
-def create_png_equivalents(src_directory, dest_directory):
-    # Check if the source directory exists
-    if not os.path.isdir(src_directory):
-        print(f"The source directory '{src_directory}' does not exist.")
-        return
-
-    # Create the destination directory if it doesn't exist
-    if not os.path.exists(dest_directory):
-        os.makedirs(dest_directory)
-
-    # Recursively traverse the source directory
-    for root, dirs, files in os.walk(src_directory):
-        # Create the corresponding subdirectories in the destination directory
-        rel_path = os.path.relpath(root, src_directory)
-        dest_subdirectory = os.path.join(dest_directory, rel_path)
-        if not os.path.exists(dest_subdirectory):
-            os.makedirs(dest_subdirectory)
-
-        # Process each file in the current directory
-        for curr_file in files:
-            src_file = os.path.join(root, curr_file)
-            dest_file = os.path.join(dest_subdirectory, os.path.splitext(curr_file)[0] + ".png")
-
-            # Convert SVG to PNG
-            from cairosvg import svg2png
-            svg2png(open(src_file, 'rb').read(), write_to=open(dest_file, 'wb'))
-
-
-# Call the method
-read_svg_files()
-# Creating png images from svg files
-create_png_equivalents("APP6-icons/svg", "APP6-icons/png")
+# Call the main method
+main()
