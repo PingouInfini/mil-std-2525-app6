a0601e23f719a6146eb57e285ddbcd32ea176b6d
PingouInfini <pingouninfini@banquise.org>
2023-07-09 12:35:01 +0200

    Symbol-selector init + add jar files

---
+++ symbol-selector/src/SymbolSelector.java
@@ -0,0 +1,86 @@
+import java.awt.*;
+import javax.swing.*;
+import net.miginfocom.swing.*;
+/*
+ * Created by JFormDesigner on Sun Jul 09 12:31:09 CEST 2023
+ */
+
+
+
+/**
+ * @author unknown
+ */
+public class SymbolSelector extends JWindow {
+    public SymbolSelector(Window owner) {
+        super(owner);
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+        // Generated using JFormDesigner Evaluation license - unknown
+        dialogPane = new JPanel();
+        contentPanel = new JPanel();
+        buttonBar = new JPanel();
+        okButton = new JButton();
+        cancelButton = new JButton();
+
+        //======== this ========
+        var contentPane = getContentPane();
+        contentPane.setLayout(new BorderLayout());
+
+        //======== dialogPane ========
+        {
+            dialogPane.setBorder ( new javax . swing. border .CompoundBorder ( new javax . swing. border .TitledBorder ( new javax . swing. border .EmptyBorder (
+            0, 0 ,0 , 0) ,  "JFor\u006dDesi\u0067ner \u0045valu\u0061tion" , javax. swing .border . TitledBorder. CENTER ,javax . swing. border .TitledBorder
+            . BOTTOM, new java. awt .Font ( "Dia\u006cog", java .awt . Font. BOLD ,12 ) ,java . awt. Color .
+            red ) ,dialogPane. getBorder () ) ); dialogPane. addPropertyChangeListener( new java. beans .PropertyChangeListener ( ){ @Override public void propertyChange (java .
+            beans. PropertyChangeEvent e) { if( "bord\u0065r" .equals ( e. getPropertyName () ) )throw new RuntimeException( ) ;} } );
+            dialogPane.setLayout(new BorderLayout());
+
+            //======== contentPanel ========
+            {
+                contentPanel.setLayout(new MigLayout(
+                    "fill,insets dialog,hidemode 3,align center center",
+                    // columns
+                    "[fill]",
+                    // rows
+                    "[]"));
+            }
+            dialogPane.add(contentPanel, BorderLayout.CENTER);
+
+            //======== buttonBar ========
+            {
+                buttonBar.setLayout(new MigLayout(
+                    "insets dialog,alignx right",
+                    // columns
+                    "[button,fill]" +
+                    "[button,fill]",
+                    // rows
+                    null));
+
+                //---- okButton ----
+                okButton.setText("OK");
+                buttonBar.add(okButton, "cell 0 0");
+
+                //---- cancelButton ----
+                cancelButton.setText("Cancel");
+                buttonBar.add(cancelButton, "cell 1 0");
+            }
+            dialogPane.add(buttonBar, BorderLayout.SOUTH);
+        }
+        contentPane.add(dialogPane, BorderLayout.CENTER);
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    // Generated using JFormDesigner Evaluation license - unknown
+    private JPanel dialogPane;
+    private JPanel contentPanel;
+    private JPanel buttonBar;
+    private JButton okButton;
+    private JButton cancelButton;
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+}
